57f4c0339a71c60ec422e28c94a2cc7ada253c7e
drillprop <drylbartosz@gmail.com>
2019-02-12 18:38:29 +0100

    add exemplary object to work with, set up basic schema and resolvers

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const resolvers = {};
+const albums = [
+  { artist: 'Black Sabbath', title: 'Paranoid', year: 1969, image: 'url' },
+  { artist: 'Iron Maiden', title: 'Killers', year: 1972, image: 'url' },
+  { artist: 'Led Zeppelin', title: 'IV', year: 1972, image: 'url' },
+  { artist: 'Cream', title: 'Disraeli Gears', year: 1968, image: 'url' },
+  {
+    artist: 'Arthur Brown',
+    title: 'Crazy World of the Arthur Brown',
+    year: 1969,
+    image: 'url'
+  }
+];
 
 const typeDefs = gql`
-  #smth
+  type Query {
+    albums: [Album]
+  }
+  type Album {
+    title: String!
+    artist: String!
+    year: Int
+    image: String
+  }
 `;
+
+const resolvers = {
+  Query: {
+    albums: () => albums
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
