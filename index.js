574a15620c6734fd1c735f736f3b19e4f9936ba2
drillprop <drylbartosz@gmail.com>
2019-02-18 21:01:38 +0100

    install mongoose, create first mongodb schema and model

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { apikey } = require('./config.js');
+const { apikey, mongoPassword } = require('./config.js');
 const fetch = require('node-fetch');
+
+const mongoose = require('mongoose');
+mongoose.connect(
+  `mongodb+srv://test:${mongoPassword}@cdmanager-92rag.mongodb.net/test?retryWrites=true`,
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // smth
+});
+
+const albumSchema = new mongoose.Schema({
+  artist: String,
+  title: String,
+  image: String,
+  id: String
+});
+
+const Album = mongoose.model('Album', albumSchema);
+const davidBowie = new Album({ artist: 'david bowie', title: 'hunky dory' });
 
 const typeDefs = gql`
   type Query {
