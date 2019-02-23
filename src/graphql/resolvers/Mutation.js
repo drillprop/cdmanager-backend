75046a87d532bff47ee65aac625fa7d72148ff02
drillprop <drylbartosz@gmail.com>
2019-02-23 11:00:47 +0100

    generate jwt

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -2,6 +2,8 @@ import fetch from 'node-fetch';
 import bcrypt from 'bcryptjs';
 import Album from '../../models/Album';
 import User from '../../models/User';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const Mutation = {
   createCd: async (parent, args, ctx, info) => {
@@ -14,6 +16,7 @@ const Mutation = {
     const { name, email, avatar } = args;
     const password = await bcrypt.hash(args.password, 10);
     const user = new User({ name, password, email, avatar });
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     await user.save();
     return user;
   }
