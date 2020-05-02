34c307c25830d116947443aba9e4dcb82a3ccb0d
drillprops <drylbartosz@gmail.com>
2020-05-02 13:09:12 +0200

    remove try catch statement from login and register resolvers

--- src/graphql/resolvers/users.js
+++ src/graphql/resolvers/users.js
@@ -17,72 +17,61 @@ export default {
   },
   Mutation: {
     register: async (_parent, args, ctx, _info) => {
-      try {
-        const { name, email, password } = args;
-        const { valid, errors } = validateRegisterInput(name, email, password);
+      const { name, email, password } = args;
+      const { valid, errors } = validateRegisterInput(name, email, password);
 
-        if (!valid) {
-          throw new UserInputError('Errors', { errors });
-        }
-
-        // check if user already exist
-        const emailExist = await User.findOne({ email });
-        if (emailExist) {
-          errors.email = 'User with this email already exist';
-          throw new UserInputError(
-            'User with this email already exist',
-            errors
-          );
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ name, email, password: hashedPassword });
-        await user.save();
-
-        // set cookie with token
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        ctx.res.cookie('token', token, {
-          httpOnly: true,
-          maxAge: 1000 * 60 * 60 * 24 * 365,
-        });
-
-        return user;
-      } catch (error) {
-        throw Error(error);
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
       }
+
+      // check if user already exist
+      const emailExist = await User.findOne({ email });
+      if (emailExist) {
+        errors.email = 'User with this email already exist';
+        throw new UserInputError('User with this email already exist', errors);
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ name, email, password: hashedPassword });
+      await user.save();
+
+      // set cookie with token
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+      ctx.res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+      });
+
+      return user;
     },
     login: async (_parent, args, ctx, _info) => {
-      try {
-        const { email, password } = args;
-        const { valid, errors } = validateLoginInput(email, password);
+      const { email, password } = args;
+      const { valid, errors } = validateLoginInput(email, password);
 
-        if (!valid) {
-          throw new UserInputError('Errors', { errors });
-        }
-
-        const user = await User.findOne({ email });
-        if (!user) {
-          errors.general = 'User not found';
-          throw new UserInputError('User not found', { errors });
-        }
-
-        const correctPasword = await bcrypt.compare(password, user.password);
-        if (!correctPasword) {
-          errors.general = 'Password is incorrect';
-          throw new UserInputError('Password is incorrect', { errors });
-        }
-
-        // set cookie with token
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        ctx.res.cookie('token', token, {
-          httpOnly: true,
-          maxAge: 1000 * 60 * 60 * 24 * 365,
-        });
-
-        return user;
-      } catch (error) {
-        throw Error(error);
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
       }
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const correctPasword = await bcrypt.compare(password, user.password);
+      if (!correctPasword) {
+        errors.general = 'Password is incorrect';
+        throw new UserInputError('Password is incorrect', { errors });
+      }
+
+      // set cookie with token
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+      ctx.res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+      });
+
+      return user;
     },
     signout: (_parent, _args, ctx, _info) => {
       ctx.res.clearCookie('token');
