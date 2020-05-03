3b93787c7cda3824ac0211191e7705a04e791602
drillprops <drylbartosz@gmail.com>
2020-05-03 18:55:29 +0200

    add rates queries and mutations to index

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,13 +1,16 @@
 import albums from './albums';
 import users from './users';
+import rates from './rates';
 
 export default {
   Query: {
     ...albums.Query,
+    ...rates.Query,
     ...users.Query,
   },
   Mutation: {
     ...albums.Mutation,
+    ...rates.Mutation,
     ...users.Mutation,
   },
 };
