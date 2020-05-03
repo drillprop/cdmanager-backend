83a861ad0e5ae9fab7654af8d72cbdc784c7d6b2
drillprops <drylbartosz@gmail.com>
2020-05-03 16:12:20 +0200

    add rates, rateCount, rateAverage fields to album schema

--- src/models/Album.js
+++ src/models/Album.js
@@ -4,6 +4,14 @@ export const albumSchema = new Schema({
   artist: String,
   title: String,
   image: String,
+  rates: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Rate',
+    },
+  ],
+  rateCount: { type: Number, default: 0 },
+  rateAverage: { type: Number, default: 0 },
   owners: [
     {
       type: Schema.Types.ObjectId,
