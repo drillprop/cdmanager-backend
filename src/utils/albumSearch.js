e2f7e6c77332ea8eaf15a0132e58283100e62854
drillprops <drylbartosz@gmail.com>
2020-05-03 13:14:53 +0200

    update albumSearch function

--- src/utils/albumSearch.js
+++ src/utils/albumSearch.js
@@ -4,65 +4,34 @@ import { Types } from 'mongoose';
 export default async (id, search) => {
   const result = await User.aggregate([
     { $match: { _id: Types.ObjectId(id) } },
-    {
-      $unwind: {
-        path: '$albums',
-      },
-    },
+    { $unwind: { path: '$albums', includeArrayIndex: 'index' } },
     {
       $lookup: {
         from: 'albums',
-        localField: 'albums.album',
+        localField: 'albums',
         foreignField: '_id',
         as: 'album',
       },
     },
+    { $unwind: { path: '$album' } },
     {
-      $unwind: {
-        path: '$album',
+      $project: {
+        _id: 0,
+        index: 1,
+        id: '$album._id',
+        title: '$album.title',
+        artist: '$album.artist',
+        image: '$album.image',
       },
     },
     {
       $match: {
         $or: [
-          {
-            'album.artist': {
-              $regex: search,
-              $options: 'i',
-            },
-          },
-          {
-            'album.title': {
-              $regex: search,
-              $options: 'i',
-            },
-          },
+          { artist: { $regex: search, $options: 'i' } },
+          { title: { $regex: search, $options: 'i' } },
         ],
       },
     },
-    {
-      $addFields: {
-        id: '$albums._id',
-        artist: '$album.artist',
-        title: '$album.title',
-        image: '$album.image',
-        rate: '$albums.rating',
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        id: 1,
-        artist: 1,
-        title: 1,
-        image: 1,
-        rate: 1,
-      },
-    },
-  ]).exec();
-
-  return result.map((album) => ({
-    ...album,
-    id: album.id.toString(),
-  }));
+  ]);
+  return result.map((album) => ({ ...album, id: album.id.toString() }));
 };
